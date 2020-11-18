9dd307bc459f056e47282ec9f01893e082a2e9f0
Abhinandan singh dahwal <70463036+AbhinandanSinghDadhwal@users.noreply.github.com>
2020-11-18 18:04:57 +0530

    edit files

---
+++ sketch.js
@@ -0,0 +1,32 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1,ball,ground;
+
+
+
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(1100,330,70,120);
+    ground = new Ground(600,height,1200,20); 
+    ball = new Ball(100,100);
+    
+}
+         
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box1.body.position.x);
+    console.log(box1.body.position.y);
+    console.log(box1.body.angle);
+    box1.display();
+    ground.display(); 
+    ball.display();
+    
+}
